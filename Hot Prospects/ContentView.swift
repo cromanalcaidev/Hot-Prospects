221fb228562583c789d19c2519c7a3b8eeeed477
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-06-04 17:30:26 +0200

    Initial Commit

---
+++ Hot Prospects/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hot Prospects
+//
+//  Created by Carlos Román Alcaide on 4/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hot Prospects/Hot_ProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_ProspectsApp.swift
+//  Hot Prospects
+//
+//  Created by Carlos Román Alcaide on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Hot_ProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
