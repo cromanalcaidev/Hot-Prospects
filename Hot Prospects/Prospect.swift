866c657904b12640cc7f19fa6378fe51b0ad7637
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-06-06 19:07:37 +0200

    Day 82, pj 16, pt. 4
    - Tabviews created
    - SwiftData implemented
    - Dynamic filtering implemented

---
+++ Hot Prospects/Prospect.swift
@@ -0,0 +1,23 @@
+//
+//  Prospect.swift
+//  Hot Prospects
+//
+//  Created by Carlos Román Alcaide on 6/6/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Prospect {
+    var name: String
+    var emailAddress: String
+    var isContacted: Bool
+    
+    init(name: String, emailAddress: String, isContacted: Bool) {
+        self.name = name
+        self.emailAddress = emailAddress
+        self.isContacted = isContacted
+    }
+}
+
---
+++ Hot Prospects/ProspectsView.swift
@@ -0,0 +1,68 @@
+//
+//  ProspectsView.swift
+//  Hot Prospects
+//
+//  Created by Carlos Román Alcaide on 6/6/24.
+//
+
+import SwiftData
+import SwiftUI
+
+struct ProspectsView: View {
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+    
+    @Environment(\.modelContext) var modelContext
+    @Query(sort: \Prospect.name) var prospects: [Prospect]
+    let filter: FilterType
+    
+    var title: String {
+        switch filter {
+        case .none:
+            "Everyone"
+        case .contacted:
+            "Contacted people"
+        case .uncontacted:
+            "Uncontacted people"
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            List(prospects) { prospect in
+                VStack(alignment: .leading) {
+                    Text(prospect.name)
+                        .font(.headline)
+                    
+                    Text(prospect.emailAddress)
+                        .foregroundStyle(.secondary)
+                }
+            }
+            .navigationTitle(title)
+            .toolbar {
+                Button("Scan", systemImage: "qrcode.viewfinder") {
+                    let prospect = Prospect(name: "Carlos", emailAddress: "carlos@apple.com", isContacted: false)
+                    modelContext.insert(prospect)
+                }
+            }
+        }
+    }
+    
+    init(filter: FilterType) {
+        self.filter = filter
+        
+        if filter != .none {
+            let showContactedOnly = filter == .contacted
+            
+            _prospects = Query(filter: #Predicate {
+                $0.isContacted == showContactedOnly
+            }, sort: [SortDescriptor(\Prospect.name)])
+        }
+    }
+}
+
+#Preview {
+    ProspectsView(filter: .none)
+        .modelContainer(for: Prospect.self)
+}
